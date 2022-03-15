fe45c122176f66623aaaecf651648caf555178c2
ingrqm <contact@imaxart.com>
2022-03-15 13:49:14 +0100

    feat: error page not authorized

--- src/views/error/not-authorized/not-authorized.tsx
+++ src/views/error/not-authorized/not-authorized.tsx
@@ -1,3 +1,17 @@
-const NotAuthorized = () => <>NotAuthorized</>;
+import { useTranslation } from 'react-i18next';
+import { Button, Result } from 'antd';
+
+const NotAuthorized = () => {
+  const { t } = useTranslation();
+
+  return (
+    <Result
+      status='403'
+      title={t('error.notAuthorized.title')}
+      subTitle={t('error.notAuthorized.subTitle')}
+      extra={<Button type='primary'>{t('error.back')}</Button>}
+    />
+  );
+};
 
 export default NotAuthorized;
