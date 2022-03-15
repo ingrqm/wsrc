b434726db629bb1334407706724231d8e6569fe3
Ingram Kalina <51913097+ingrqm@users.noreply.github.com>
2022-03-15 13:49:41 +0100

    feat: error page not authorized (#23)

--- src/locales/en/error.ts
+++ src/locales/en/error.ts
@@ -4,4 +4,8 @@ export default {
     title: '404',
     subTitle: 'Sorry, the page you visited does not exist.',
   },
+  notAuthorized: {
+    title: '403',
+    subTitle: 'Sorry, you are not authorized to access this page.',
+  },
 };
