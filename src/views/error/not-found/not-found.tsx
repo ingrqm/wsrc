f26c9b8069e847d1f997bf0629f67709e93fd3a0
ingrqm <contact@imaxart.com>
2022-03-15 13:43:53 +0100

    feat: page error not found

--- src/views/error/not-found/not-found.tsx
+++ src/views/error/not-found/not-found.tsx
@@ -1,3 +1,17 @@
-const NotFound = () => <>NotFound</>;
+import { useTranslation } from 'react-i18next';
+import { Button, Result } from 'antd';
+
+const NotFound = () => {
+  const { t } = useTranslation();
+
+  return (
+    <Result
+      status='404'
+      title={t('error.notFound.title')}
+      subTitle={t('error.notFound.subTitle')}
+      extra={<Button type='primary'>{t('error.back')}</Button>}
+    />
+  );
+};
 
 export default NotFound;
