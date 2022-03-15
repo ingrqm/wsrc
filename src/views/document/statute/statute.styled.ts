3e5f3fbfa18f7ba98adac2308c71e310bb18ec79
ingrqm <contact@imaxart.com>
2022-03-15 16:59:30 +0100

    feat: page document statute

---
+++ src/views/document/statute/statute.styled.ts
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+export const StyledList = styled.ul`
+  max-width: 450px;
+  list-style: circle;
+  margin-top: 24px;
+`;
+
+export const StyledListItem = styled.li`
+  font-size: 14px;
+  line-height: 22px;
+  font-weight: 400;
+  margin-bottom: 24px;
+`;
