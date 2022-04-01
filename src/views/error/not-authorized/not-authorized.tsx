5dbdb8ea5af1dd0c6f216321e7fdd98b4fb30e23
ingrqm <contact@imaxart.com>
2022-04-01 11:42:41 +0200

    feat: navigation on error pages

--- src/views/error/not-authorized/not-authorized.tsx
+++ src/views/error/not-authorized/not-authorized.tsx
@@ -1,15 +1,33 @@
 import { useTranslation } from 'react-i18next';
+import { useNavigate } from 'react-router-dom';
 import { Button, Result } from 'antd';
+import { userAtom } from 'atoms/user';
+import { useRecoilValue } from 'recoil';
+import { appUrls } from 'urls';
 
 const NotAuthorized = () => {
   const { t } = useTranslation();
+  const navigate = useNavigate();
+  const user = useRecoilValue(userAtom);
+
+  const handleBackToApp = () => {
+    if (user.isLoggedIn) {
+      navigate(appUrls.app.dashboard);
+    } else {
+      navigate(appUrls.auth.signIn);
+    }
+  };
 
   return (
     <Result
       status='403'
       title={t('error.notAuthorized.title')}
       subTitle={t('error.notAuthorized.subTitle')}
-      extra={<Button type='primary'>{t('error.back')}</Button>}
+      extra={
+        <Button type='primary' onClick={handleBackToApp}>
+          {t('error.back')}
+        </Button>
+      }
     />
   );
 };
--- src/views/error/not-found/not-found.tsx
+++ src/views/error/not-found/not-found.tsx
@@ -1,15 +1,33 @@
 import { useTranslation } from 'react-i18next';
+import { useNavigate } from 'react-router-dom';
 import { Button, Result } from 'antd';
+import { userAtom } from 'atoms/user';
+import { useRecoilValue } from 'recoil';
+import { appUrls } from 'urls';
 
 const NotFound = () => {
   const { t } = useTranslation();
+  const navigate = useNavigate();
+  const user = useRecoilValue(userAtom);
+
+  const handleBackToApp = () => {
+    if (user.isLoggedIn) {
+      navigate(appUrls.app.dashboard);
+    } else {
+      navigate(appUrls.auth.signIn);
+    }
+  };
 
   return (
     <Result
       status='404'
       title={t('error.notFound.title')}
       subTitle={t('error.notFound.subTitle')}
-      extra={<Button type='primary'>{t('error.back')}</Button>}
+      extra={
+        <Button type='primary' onClick={handleBackToApp}>
+          {t('error.back')}
+        </Button>
+      }
     />
   );
 };
