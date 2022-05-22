e2976b62fcbfbdaceace53ef54017bed8d8526a1
ingrqm <contact@imaxart.com>
2022-05-23 00:53:37 +0200

    feat: tutorial language fix

--- src/views/app/dashboard/dashboard.utils.ts
+++ src/views/app/dashboard/dashboard.utils.ts
@@ -5,23 +5,19 @@ export const getTutorialUrl = (permission: Permission, languageChampionship: Lan
     case Permission.newbie:
     case Permission.user:
       switch (languageChampionship) {
-        case LanguageChampionship.en:
-          return 'https://player.vimeo.com/video/704165934?h=4681a3fccb&title=0&byline=0&portrait=0';
         case LanguageChampionship.fr:
           return 'https://player.vimeo.com/video/704166182?h=0fbd851c3f&title=0&byline=0&portrait=0';
         default:
-          return '';
+          return 'https://player.vimeo.com/video/704165934?h=4681a3fccb&title=0&byline=0&portrait=0';
       }
     case Permission.arbiter:
     case Permission.admin:
     case Permission.superAdmin:
       switch (languageChampionship) {
-        case LanguageChampionship.en:
-          return 'https://player.vimeo.com/video/704165529?h=870198a57f&title=0&byline=0&portrait=0';
         case LanguageChampionship.fr:
           return 'https://player.vimeo.com/video/704165342?h=59eae9d0a0&title=0&byline=0&portrait=0';
         default:
-          return '';
+          return 'https://player.vimeo.com/video/704165529?h=870198a57f&title=0&byline=0&portrait=0';
       }
     default:
       return '';
